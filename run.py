fd89490b1a512933302e3ab57ce0ed2e3e5f63cc
floordiv <pavloromanenko25@gmail.com>
2020-06-13 12:48:38 +0300

    run.py updated and now can be used

--- run.py
+++ run.py
@@ -1,7 +1,13 @@
 import sys
-import interpreter.interpreter as l
+
+sys.path.append('interpreter/')
+
+if '/' in sys.argv[0]:
+    sys.path.append(sys.argv[0])
+
+import interpreter.interpreter as lang
 
 
 file = sys.argv[1]
 
-l.run(file)
+lang.run(file)
